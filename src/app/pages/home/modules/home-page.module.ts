224d2cb8e6c790fe03d49143df63aceb780aff0d
gitbrains <gitbrains@gmail.com>
2023-06-18 23:49:35 +0500

    build: github pages

--- src/app/pages/home/modules/home-page.module.ts
+++ src/app/pages/home/modules/home-page.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from '../components/home-page/home-page.component';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { LayoutModule } from 'src/app/features/layout/modules/layout.module';
 
 @NgModule({
   declarations: [HomePageComponent],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    LayoutModule
   ],
   exports: [HomePageComponent]
 })
